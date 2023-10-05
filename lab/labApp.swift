e37246e5bbefb967f823ea9f3c8c94b4b17afe09
mnerahawn <mnerahawncs@gmail.com>
2023-10-05 12:41:01 +0300

    Initial Commit

---
+++ lab/labApp.swift
@@ -0,0 +1,17 @@
+//
+//  labApp.swift
+//  lab
+//
+//  Created by mnerah awn on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct labApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
